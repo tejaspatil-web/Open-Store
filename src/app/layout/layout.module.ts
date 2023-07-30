45524c34b85dfb85d6eb4a89d7768f4fb6644d5f
Tejas Patil <tejasp094@gmail.com>
2023-07-30 15:44:45 +0530

    some changes

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentLayoutComponent } from './content-layout/content-layout.component';
 
 @NgModule({
   declarations: [
+    HeaderComponent,
+       FooterComponent,
+       ContentLayoutComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[ ]
+  exports:[ 
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class LayoutModule { }
