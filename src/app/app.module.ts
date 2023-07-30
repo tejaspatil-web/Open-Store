535f3dbca403bac9d7b46a7a2de60eea3486dd18
Tejas Patil <tejasp094@gmail.com>
2023-07-30 15:18:23 +0530

    app structure half done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import {MatModule} from './modules/material/mat.module'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
