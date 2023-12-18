c6deb1b4c1dc6de67ab38bacf833b132a783a8f4
Tejas Patil <tejasp094@gmail.com>
2023-12-19 01:44:22 +0530

    unwanted code removed

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/api-url-environment';
-import { userAuthenticationModel , userDetails} from '../../../models/user-auth.model';
+import {
+  userAuthenticationModel,
+  userDetails,
+} from '../../../models/user-auth.model';
 
 @Injectable({
   providedIn: 'root',
@@ -18,36 +21,32 @@ export class UserService {
     return this._http.get(`${this._url.userApi}/user/${id}`);
   }
 
-  userRegistration(userDetails:userDetails){
-    return this._http.post(`${this._url.userApi}/create`,userDetails)
+  userRegistration(userDetails: userDetails) {
+    return this._http.post(`${this._url.userApi}/create`, userDetails);
   }
 
-  sendOtpForEmailVerification(email:Object){
-    return this._http.post(`${this._url.userApi}/sendotp`,email)
+  sendOtpForEmailVerification(email: Object) {
+    return this._http.post(`${this._url.userApi}/sendotp`, email);
   }
 
-  userEmailVerification(email:String,otp:String){
-    const parameters = {email:email,otp:otp}
-    return this._http.post(`${this._url.userApi}/verifyotp`,parameters)
+  userEmailVerification(email: String, otp: String) {
+    const parameters = { email: email, otp: otp };
+    return this._http.post(`${this._url.userApi}/verifyotp`, parameters);
   }
 
-  sendOtpResetPassword(email:String){
-    const url = 'http://localhost:8080/api/user/forgotpassword'
-    // return this._http.post(`${this._url.userApi}/forgotpassword`,{email:email})
-    return this._http.post(`${url}`,{email:email})
+  sendOtpResetPassword(email: String) {
+    return this._http.post(`${this._url.userApi}/forgotpassword`, {
+      email: email,
+    });
   }
 
-otpVerifiactionResetPassword(email:String,otp:String){
-  const parameters = {email:email,otp:otp}
-  const url = 'http://localhost:8080/api/user/otpverification'
-  // return this._http.post(`${this._url.userApi}/otpverification`,parameters)
-  return this._http.post(`${url}`,parameters)
-}
+  otpVerifiactionResetPassword(email: String, otp: String) {
+    const parameters = { email: email, otp: otp };
+    return this._http.post(`${this._url.userApi}/otpverification`, parameters);
+  }
 
-updatePassword(email:String,password:String,otp:String){
-  const parameters = {email:email,password:password,otp:otp}
-  const url = 'http://localhost:8080/api/user/updatepassword'
-  return this._http.post(`${url}`,parameters)
-}
-
+  updatePassword(email: String, password: String, otp: String) {
+    const parameters = { email: email, password: password, otp: otp };
+    return this._http.post(`${this._url.userApi}/updatepassword`, parameters);
+  }
 }
